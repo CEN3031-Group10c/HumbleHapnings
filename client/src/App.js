82baf057acad3dbb1ac9ca403d081768e4ff65d7
Kevin Wang <kevinwang@Kevins-MacBook-Pro.local>
2019-11-01 07:02:32 -0400

    added more routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
-import Header from "./components/Header/Header"
+import Register from "./views/Register/Register"
+import Login from "./views/Login/Login"
 
 
 const App = () => {
   return (
-    <div>
-      <Header />
+    <div> 
       <Switch>
+        <Route exact path="/Login" component={Login}/>
+        <Route exact path="/Register" component={Register}/> 
         <Route exact path="/Home" component={Home} />
         <Route exact path="/">
-          <Redirect to="/Home" />
+          <Redirect to="/Login" />
         </Route>
         <Route component={NotFound}/>
       </Switch>
