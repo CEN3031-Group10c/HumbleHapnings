6839455249556d100dbd2e73950186fbe7cbfac0
ikevinws <ikevinwang@ufl.edu>
2019-11-05 08:22:33 -0500

    added comments

--- client/src/views/Home/Home.js
+++ client/src/views/Home/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Home.css';
 import Header from "../../components/Header/Header.js"
+
+//From: Kevin
+/*
+this function randomly picks a background 
+and returns the classname of the background image in css
+*/
 function randomImage(){
     var backgroundImages = ['book1-background', 'cathedral1-background', 'field1-background'];
     var randomIndex = Math.floor(Math.random() * backgroundImages.length);
     var randomImg = backgroundImages[randomIndex];
     return randomImg;
 }    
+//To: Kevin
 
 class Home extends React.Component {
 
