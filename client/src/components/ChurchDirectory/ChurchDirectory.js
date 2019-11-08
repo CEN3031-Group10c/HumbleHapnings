b38f971eff6c93953b98578aa683679504cc393d
Cluekas <lugilmir@gmail.com>
2019-11-08 14:05:57 -0500

    Began front-end development of church directory

---
+++ client/src/components/ChurchDirectory/ChurchDirectory.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ChurchDirectory.css';
+
+
+class ChurchDirectory extends React.Component {
+    render(){
+        return (
+            <div>
+                <p>Hello i am directory</p>
+            </div>
+        )
+    }
+}
+
+export default ChurchDirectory;
