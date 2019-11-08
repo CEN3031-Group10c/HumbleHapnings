069d02cf8cc4799c730b70ce44fe6d02f364e2af
diegosimonelli <dsimonelli@ufl.edu>
2019-11-08 13:00:53 -0500

    Diego and Sebastian events page in process

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from "./views/Home/Home"
 import NotFound from "./views/NotFound"
 import Register from "./views/Register/Register"
 import Login from "./views/Login/Login"
+import Events from "./views/Events/Events"
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Route exact path="/Login" component={Login}/>
         <Route exact path="/Register" component={Register}/> 
         <Route exact path="/Home" component={Home} />
+        <Route exact path="/Events" component={Events}></Route>
         <Route exact path="/">
           <Redirect to="/Login" />
         </Route>
