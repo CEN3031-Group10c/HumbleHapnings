c3d3a4c4e4e0d126847f0d6ff9248cf3de1686fc
mfowler38 <fowler.megan@hotmail.com>
2019-11-08 23:56:27 -0500

    completed steps up to and including step vi
    Followed tutorial almost exactly except User Schema has additional field for paidUser and the mongodb connection is slightly different than in the tutorial but that should not have to be touched

--- server/server.js
+++ server/server.js
@@ -1,5 +1,11 @@
-const express = require('./config/express.js')
- 
+const express = require('./config/express.js');
+const mongoose = require("mongoose");
+//const passport = require("passport");
+const config = require ('./config/config')
+
+ // Connect to database
+ mongoose.connect(config.db.uri, { useNewUrlParser: true });
+
 // Use env port or default
 const port = process.env.PORT || 5000;
 
