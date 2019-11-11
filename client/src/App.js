2dd68b1630a1c2fac35ab24cacb7f7325c390a99
joshuadargan <joshuadargan282@ufl.edu>
2019-11-11 11:32:50 -0500

    Register changes and added a testing route to demonstrate potential UI changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Route, Switch, Redirect  } from 'react-router-dom';
-import Home from "./views/Home/Home"
-import NotFound from "./views/NotFound"
-import Register from "./views/Register/Register"
-import Login from "./views/Login/Login"
+import Home from "./views/Home/Home";
+import NotFound from "./views/NotFound";
+import Register from "./views/Register/Register";
+import Login from "./views/Login/Login";
 
 import { Provider } from "react-redux";
 import store from "./store";
+import Navbar from './components/layout/Navbar';
+import Landing from "./components/layout/Landing";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div> 
         <Switch>
+          <Route exact path="/Test" component={Navbar,Landing}/>
           <Route exact path="/Login" component={Login}/>
           <Route exact path="/Register" component={Register}/> 
           <Route exact path="/Home" component={Home} />
