dfbe6b6a0f3797e2c3cf00210bd98084dd9c506c
joshuadargan <joshuadargan282@ufl.edu>
2019-11-12 14:03:44 -0500

    Header changes

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -9,22 +9,40 @@ class Header extends React.Component {
         const {selectedUpdate} = this.props;
 
         return (
-            <div>
-                <body className='header'>
-                    <div className='navbar'>
-                    <li className='logo'>HumbleHapnings</li>
-                    <ul class="nav">
-                        <button onClick={() => selectedUpdate(0)}>Home</button>
-                        <button onClick={() => selectedUpdate(1)}>About</button>
-                        <button onClick={() => selectedUpdate(2)}>Events</button>
-                        <button onClick={() => selectedUpdate(3)}>Church Directory</button>
-                        <Link to="/ChurchCreation">
-                            <button onClick={() => selectedUpdate(4)}>Create Church Directory</button>
+
+        <nav className="z-depth-0">
+            <div className="nav-wrapper white">
+                <a href="#" class="logo"> <Link
+                        to="/"
+                        style={{
+                            fontFamily: "monospace"
+                        }}
+                        className="col s5 brand-logo left black-text"
+                    >
+                            <i className="material-icons">flare</i>
+                            HumbleHapnings
                         </Link>
-                    </ul>
-                    </div>
-                </body>
+                </a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li>
+                        <a class="waves-effect waves-light btn" onClick={() => selectedUpdate(0)}>Home</a>
+                    </li>
+                    <li>
+                        <a class="waves-effect waves-light btn" onClick={() => selectedUpdate(1)}>About</a>
+                    </li>
+                    <li>
+                        <a class="waves-effect waves-light btn" onClick={() => selectedUpdate(2)}>Events</a>
+                    </li>
+                    <li>
+                        <a class="waves-effect waves-light btn" onClick={() => selectedUpdate(3)}>Events</a>
+                    </li>
+                    <li>
+                        <a class="waves-effect waves-light btn"onClick={() => selectedUpdate(4)}><Link to="/ChurchCreation">Create Church Directory</Link></a>
+                    </li>
+                </ul>
             </div>
+      </nav>
         )
     }
 }
