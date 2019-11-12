2f3353c4ece0afe985fb3aaf637e2df85be3b694
joshuadargan <joshuadargan282@ufl.edu>
2019-11-12 14:03:44 -0500

    Header changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,22 +6,26 @@ import { Link } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
-      <div className="navbar-fixed">
+
         <nav className="z-depth-0">
-          <div className="nav-wrapper white">
-            <Link
+        <div className="nav-wrapper white">
+          <Link
               to="/"
               style={{
                 fontFamily: "monospace"
               }}
-              className="col s5 brand-logo center black-text"
+              className="col s5 brand-logo left black-text"
             >
               <i className="material-icons">flare</i>
               HumbleHapnings
             </Link>
-          </div>
-        </nav>
-      </div>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
