e23bda8622042be5ca2ec6dfc6dfd5b7b61e2a29
Cluekas <lugilmir@gmail.com>
2019-11-26 11:27:10 -0500

    Minor CSS changes

--- client/src/views/ChurchDirectoryCreation/ChurchDirectoryCreation.js
+++ client/src/views/ChurchDirectoryCreation/ChurchDirectoryCreation.js
@@ -115,114 +115,116 @@ class ChurchDirectoryCreation extends React.Component{
     render(){
         return(
             <div>
-                <div className="churchCreationContainer">
-                    <body className="col s8 offset-s2 colorDiv2">
-                        <div className="churchCreationPad">
-                            <div class="row-container">
-                                <div class="column2">
-                                    <form onSubmit = {this.onSubmit}>
-                                        <p>Please fill in this form to create a church listing.</p>
-                                        <div class="regName">
-                                            <label for="regName"><b>Name</b></label>
-                                            <input 
-                                                id="name" 
-                                                type="text" 
-                                                placeholder="Enter Name of Church" 
-                                                value= {this.state.name}
-                                                onChange={this.onChange}
-                                                required
-                                            />
+                <Header/>
+                    <div className="churchCreationContainer">
+                        <body className="col s8 offset-s2 colorDiv2">
+                            <div className="churchCreationPad">
+                                <h1>Create a Church Listing</h1>
+                                <div class="row-container">
+                                    <div class="column2">
+                                        <form onSubmit = {this.onSubmit}>
+                                            <p>Please fill in this form to create a church listing.</p>
+                                            <div class="regName">
+                                                <label for="regName"><b>Name</b></label>
+                                                <input 
+                                                    id="name" 
+                                                    type="text" 
+                                                    placeholder="Enter Name of Church" 
+                                                    value= {this.state.name}
+                                                    onChange={this.onChange}
+                                                    required
+                                                />
 
-                                            <label for="regPastor"><b>Pastor</b></label>
-                                            <input 
-                                                id="pastor" 
-                                                type="text" 
-                                                placeholder="Enter Pastor Name" 
-                                                value= {this.state.pastor}
-                                                onChange={this.onChange}
-                                                required
-                                            />
-                                            
-                                            <label for="regEmail"><b>Email</b></label>
-                                            <input 
-                                                id="email" 
-                                                type="text" 
-                                                placeholder="Enter Church Email" 
-                                                value= {this.state.email}
-                                                onChange={this.onChange}
-                                                required
-                                            />
+                                                <label for="regPastor"><b>Pastor</b></label>
+                                                <input 
+                                                    id="pastor" 
+                                                    type="text" 
+                                                    placeholder="Enter Pastor Name" 
+                                                    value= {this.state.pastor}
+                                                    onChange={this.onChange}
+                                                    required
+                                                />
+                                                
+                                                <label for="regEmail"><b>Email</b></label>
+                                                <input 
+                                                    id="email" 
+                                                    type="text" 
+                                                    placeholder="Enter Church Email" 
+                                                    value= {this.state.email}
+                                                    onChange={this.onChange}
+                                                    required
+                                                />
 
-                                            <label for="regAddress"><b>Address</b></label>
-                                            <input 
-                                                id="address" 
-                                                type="text" 
-                                                placeholder="Enter Church Address"
-                                                value= {this.state.address} 
-                                                onChange={this.onChange}
-                                                required
-                                            />  
+                                                <label for="regAddress"><b>Address</b></label>
+                                                <input 
+                                                    id="address" 
+                                                    type="text" 
+                                                    placeholder="Enter Church Address"
+                                                    value= {this.state.address} 
+                                                    onChange={this.onChange}
+                                                    required
+                                                />  
 
-                                            <label for="regPhone"><b>Phone Number</b></label>
-                                            <input 
-                                                id="phone" 
-                                                type="text" 
-                                                placeholder="Enter Church Phone Number" 
-                                                value= {this.state.phone}
-                                                onChange={this.onChange}
-                                                required
-                                            />
+                                                <label for="regPhone"><b>Phone Number</b></label>
+                                                <input 
+                                                    id="phone" 
+                                                    type="text" 
+                                                    placeholder="Enter Church Phone Number" 
+                                                    value= {this.state.phone}
+                                                    onChange={this.onChange}
+                                                    required
+                                                />
 
-                                            <label for="regDenomination"><b>Denomination</b></label>
-                                            <input 
-                                                id="denomination" 
-                                                type="text" 
-                                                placeholder="Enter the Church's Denomination" 
-                                                value= {this.state.denomination}
-                                                onChange={this.onChange}
-                                                required
-                                            />
+                                                <label for="regDenomination"><b>Denomination</b></label>
+                                                <input 
+                                                    id="denomination" 
+                                                    type="text" 
+                                                    placeholder="Enter the Church's Denomination" 
+                                                    value= {this.state.denomination}
+                                                    onChange={this.onChange}
+                                                    required
+                                                />
 
-                                            <label for="regMissionStatement"><b>Mission Statement</b></label>
-                                            <input 
-                                                id="missionStatement" 
-                                                type="text" 
-                                                placeholder="Enter the Mission Statement of the Church" 
-                                                value= {this.state.missionStatement}
-                                                onChange={this.onChange}
-                                                required
-                                            />
+                                                <label for="regMissionStatement"><b>Mission Statement</b></label>
+                                                <input 
+                                                    id="missionStatement" 
+                                                    type="text" 
+                                                    placeholder="Enter the Mission Statement of the Church" 
+                                                    value= {this.state.missionStatement}
+                                                    onChange={this.onChange}
+                                                    required
+                                                />
 
-                                            <label for="regDescription"><b>Description</b></label>
-                                            <input 
-                                                id="description" 
-                                                type="text" 
-                                                placeholder="Enter a short description of the church" 
-                                                value= {this.state.description}
-                                                onChange={this.onChange}
-                                            />
-                                            <label for="regImage"><b>Church Image   </b></label>
-                                            <input 
-                                                id="image" 
-                                                type="file"
-                                                onChange={this.fileSelectedHandler}
-                                            />
-                                        </div>
+                                                <label for="regDescription"><b>Description</b></label>
+                                                <input 
+                                                    id="description" 
+                                                    type="text" 
+                                                    placeholder="Enter a short description of the church" 
+                                                    value= {this.state.description}
+                                                    onChange={this.onChange}
+                                                />
+                                                <label for="regImage"><b>Church Image   </b></label>
+                                                <input 
+                                                    id="image" 
+                                                    type="file"
+                                                    onChange={this.fileSelectedHandler}
+                                                />
+                                            </div>
 
-                                        <button 
-                                            type="submit" 
-                                            class="reg"
-                                            // Makes it so they can't submit while the image is uploading
-                                            disabled={ this.state.imageUpload && (!this.state.imageUploadDone) }
-                                        >
-                                            Create Church Listing
-                                        </button>
-                                    </form>
+                                            <button 
+                                                type="submit" 
+                                                class="reg"
+                                                // Makes it so they can't submit while the image is uploading
+                                                disabled={ this.state.imageUpload && (!this.state.imageUploadDone) }
+                                            >
+                                                Create Church Listing
+                                            </button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </body>
-                </div>
+                        </body>
+                    </div>
             </div>
         )
     }
