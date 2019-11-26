a5a1e6f86462d8e66b8b261c17c30ba599bc2973
ikevinws <ikevinwang@ufl.edu>
2019-11-25 19:44:01 -0500

    close and send confirmation to eventcreation.js

--- client/src/components/CheckoutForm/CheckoutForm.js
+++ client/src/components/CheckoutForm/CheckoutForm.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {CardElement, injectStripe} from 'react-stripe-elements';
 import axios from 'axios';
 import './CheckoutForm.css'
+import {Link} from 'react-router-dom';
+
 
 const createOptions = () => {
     return {
@@ -30,6 +32,28 @@ class CheckOutForm extends React.Component{
             errorMessage: '',
         };
     }
+    //updating state of parent's show (in EventCreation)
+    onClose = (event) => {
+        this.props.onClose && this.props.onClose(event)
+    }
+
+    //updating state of parent's purchaseComplete after the backend of stripe sends confirmation
+    onPurchase = (event) =>{
+        //if payments has been processed by stripe, return true to parent component
+        if (this.state.complete === true){
+            this.props.onPurchase && this.props.onPurchase(true)
+        }
+        //else return false to parent
+        else{
+            this.props.onPurchase && this.props.onPurchase(false)
+        }
+    }
+
+    makeAnotherEvent = (event)=>{
+        this.props.makeAnotherEvent && this.props.makeAnotherEvent(event)
+    }
+
+
     //if user clicked submit
     async handleSubmit(event){
         event.preventDefault();
@@ -45,10 +69,13 @@ class CheckOutForm extends React.Component{
             name,
             email,
         });
-        
         const {status} = response.data;
+        console.log("stripe status",status);
         if (status === 'success'){
             this.setState({complete: true});
+            //if successful send back purchase confirmation to eventcreation.js (parent component)
+            this.onPurchase()
+            
         }
     }
 
@@ -59,57 +86,78 @@ class CheckOutForm extends React.Component{
     };
 
 
+
     render(){
-        if (this.state.complete){
-            return (
-                <div className="checkout-card">
-                    <h1> Purchase Complete </h1>
-                </div>
-            )
+        if(!this.props.show){
+            return null;
         }
         else{
-            return(
-                <div className="checkout-card">
-                    <form onSubmit={this.handleSubmit.bind(this)}>
-                        <div>
-                            <label>
-                                Name
-                                <input
-                                    name="name"
-                                    type="text"
-                                    placeholder="Jane Smith"
-                                    className="checkoutInput"
-                                    required
-                                />
-                            </label>
-                            <label>
-                                Email
-                                <input
-                                    name="email"
-                                    type="email"
-                                    placeholder="example@example.com"
-                                    className="checkoutInput"
-                                    required
-                                />
-                            </label>
+            if(!this.state.complete){
+                return(
+                    <div className="checkout-bg-modal">
+                        <div className="checkout-card">
+                            <div className="checkout-content">
+                                <div className="checkout-close" id="checkout-close" onClick={(event) => {this.onClose(event)}}>+</div>
+                                <form onSubmit={this.handleSubmit.bind(this)}>
+                                    <div>
+                                        <label>
+                                            Name
+                                            <input
+                                                name="name"
+                                                type="text"
+                                                placeholder="Jane Smith"
+                                                className="checkoutInput"
+                                                required
+                                            />
+                                        </label>
+                                        <label>
+                                            Email
+                                            <input
+                                                name="email"
+                                                type="email"
+                                                placeholder="example@example.com"
+                                                className="checkoutInput"
+                                                required
+                                            />
+                                        </label>
+                                    </div>
+                                    <label>
+                                        Card details
+                                        <CardElement 
+                                            {...createOptions()}
+                                            onChange={this.handleChange}
+                                        />
+                                    </label>
+                                    <div className="error" role="alert">
+                                        {this.state.errorMessage}
+                                    </div>
+                                    <div>
+                                        <button className="checkoutButton">Create Event For $1</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <label>
-                            Card details
-                            <CardElement 
-                                {...createOptions()}
-                                onChange={this.handleChange}
-                            />
-                        </label>
-                        <div className="error" role="alert">
-                            {this.state.errorMessage}
+                    </div>
+                )
+            }
+            else {
+                return (
+                    <div className="checkout-bg-modal">
+                        <div className="checkout-card-complete">
+                        <div className="checkout-close" id="checkout-close" onClick={(event) => {this.onClose(event)}}>+</div>
+                            <div>
+                                <h3> Purchase Completed </h3>
+                                <div checkout-button-box>
+                                <button className="checkoutButton-complete" onClick={(event) => {this.makeAnotherEvent(event)}}>Create Another Event</button>
+                                <Link to="/Events"><button className="checkoutButton-complete">See Events</button></Link>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <button className="checkoutButton">Create Event For $1</button>
-                        </div>
-                    </form>
-                </div>
-            )
+                    </div>
+                )
+            }
         }
+        
     }
 
 }
