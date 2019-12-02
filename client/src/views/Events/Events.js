69c8f0bf6ed311796a266f7933fa61978bc611f4
shebasquine <40399062+llerenabarruetos@users.noreply.github.com>
2019-12-02 12:38:12 -0500

    stable events page

--- client/src/views/Events/Events.js
+++ client/src/views/Events/Events.js
@@ -5,8 +5,6 @@ import {Row} from 'reactstrap';
 import axios from 'axios';
 import "../Home/Home.css"
 import {Link} from 'react-router-dom';
-import SelectBoxMenu from './dropDown';
-import Search from './Search.js';
 
 
 // Sebastian and Diego
@@ -23,27 +21,9 @@ class Events extends React.Component{
     constructor (){
         super();
         this.state = {
-            eventListings: [],
-            typeOfInputForSearch: "text",
-            searchFilter: "name",
-            filterText: '',
+            eventListings: []
         };
     }
-
-    filterUpdate(value) {
-        //For filter
-        this.setState({
-          filterText: value
-        });
-      }
-
-    updateFilter(item) {
-        this.setState({
-            searchFilter: item.value
-        });
-        
-    }
-
     // Retrieves listings from the backend
     componentDidMount() {
         axios.get('api/EventCreation/list').then(res => {
@@ -60,7 +40,6 @@ class Events extends React.Component{
         description: "Discussing upcoming fair",
         hostChurch: "Our-Church",
         tags: ["Discussion, ", "Social commitment, ", "Planning"]},
-
         {name: "6 PM Sunday Mass",
         date: "11/13",
         location: "Main Church",
@@ -80,36 +59,15 @@ class Events extends React.Component{
 
         //mapping the elements of the array
         //outline 
-        const eventMaping = this.state.eventListings
-        .filter(eventListing => {
-                if (this.state.searchFilter == "name")
-                    return   eventListing.name.toLowerCase().indexOf(this.state.filterText.toLowerCase()) >= 0
-                if (this.state.searchFilter == "date")
-                    return   eventListing.date.toLowerCase().indexOf(this.state.filterText.toLowerCase()) >= 0
-                if (this.state.searchFilter == "location")
-                    return   eventListing.location.toLowerCase().indexOf(this.state.filterText.toLowerCase()) >= 0
-                if (this.state.searchFilter == "hostChurch")
-                    return   eventListing.hostChurch.toLowerCase().indexOf(this.state.filterText.toLowerCase()) >= 0
-                if (this.state.searchFilter == "tag" && eventListing.tags[0] != null)
-                {
-                    for (const [index, value] of eventListing.tags.entries())
-                    {
-                        console.log("index: " + index)
-                        console.log("value: " + value)
-                        if( eventListing.tags[index].toLowerCase().indexOf(this.state.filterText.toLowerCase()) >= 0 )
-                            return eventListing.tags[index].toLowerCase().indexOf(this.state.filterText.toLowerCase()) >= 0
-                       
-                    }
-                }
-        })
-        .map(event => {
+        const eventMaping = this.state.eventListings.map(event => {
             return (
-                <div class="columnEvents" style={{display: 'flex'}, {padding: '0px'}, {paddingTop: '0px'}, {justifyContent:'center', alignItems:'center'}}>
-                    <div class="containerEvent">
+                <div className="columnEvents" style={{display: 'flex'}, {padding: '0px'}, {paddingTop: '0px'}, {justifyContent:'center', alignItems:'center'}, {paddingBottom: '50px'}}>
+                    <div className="containerEvent">
                         <text style={{fontSize: 45}}>{event.name}</text>
                         <tr style={{fontSize: 23}}>By: {event.hostChurch}</tr>
-                        <tr style={{fontSize: 19}}>{event.location}</tr>
-                        <tr style={{fontSize: 19}}>{event.date}</tr>
+                        <tr style={{fontSize: 19}}>Location: {event.location}</tr>
+                        <tr style={{fontSize: 19}}>Date: {event.date}</tr>
+                        <tr style={{fontSize: 19}}>Time: {event.time}</tr>
                         <tr style={{fontSize: 19}}>Description: {event.description}</tr>
                         <tr style={{fontSize: 19}}>Related to: {event.tags.map ( tag => {
                             return (
@@ -118,23 +76,22 @@ class Events extends React.Component{
                         })}
                         </tr>
                     </div>  
+                    <div style={{paddingBottom: '30px'}}>{"   "}</div>   
                 </div>            
         );
     });
 
     var letterColor = "black";
-        //organizing page components
-
-        
+        //organazing page components
         // Megan - worked on code formatting plus changed return to proper format to work with linking to webpages vs. 
-        // puting component over top of home page
+        // putting component over top of home page
 
         return (
             <div>
                 <Header/>
                 <body className="Events">
                     <div className={randomImage()}> 
-                        <div class="container">
+                        <div className="container">
                             <h1 style={{color: letterColor, fontSize: 70,  display: 'flex',  justifyContent:'center', alignItems:'center'}}>Events</h1>
                             
                             <text style={{color: letterColor, fontSize: 30,  display: 'flex',  justifyContent:'center', alignItems:'center'}}>Check out these awesome events happening in your community!</text>
@@ -150,32 +107,9 @@ class Events extends React.Component{
                                     
                                     <div className = "columnB">
                                         <div className = "rightSide">
-                                            <Link to="/EventCreation"><button style={{paddingBottom: '15px'},{borderRadius: '20px'}}> Create an Event</button></Link>
-                                            
-                                            Search:
-                                            <Search filterText={this.state.filterText}
-                                                    filterOn = {this.state.searchFilter}
-                                                    filterUpdate={this.filterUpdate.bind(this)}
-                                            />
-
-
-                                            Filter On:
-                                            <SelectBoxMenu
-                                                width={150}
-                                                items = {[
-                                                    {value: 'name', id: 1 },
-                                                    {value: 'location', id: 2 },
-                                                    {value: 'date', id: 3 },
-                                                    {value: 'hostChurch', id: 4 },
-                                                    {value: 'tag', id: 5 }
-                                                ]}
-                                                updateFilter = {this.updateFilter.bind(this)}
-                                                
-                                             />
-                                        
-
+                                            Place for Buttons and search bar
+                                            <Link to="/EventCreation"><button> Create an Event</button></Link>
                                         </div>
-                                        
                                         
                                     </div>
                                 </Row>
