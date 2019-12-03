ab843aa6867433686da8f0c075f2d2fed4034081
mfowler38 <fowler.megan@hotmail.com>
2019-12-03 07:24:26 -0500

    Church Leader Header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
-import { ADMIN } from "../../actions/userTypes";
+import { ADMIN, CHURCH_LEADER } from "../../actions/userTypes";
 
 //https://www.w3schools.com/css/css_navbar.asp
 class Header extends React.Component {
@@ -15,7 +15,7 @@ class Header extends React.Component {
 
     render(){
 
-        var adminHeader, regularHeader, displayHeader;
+        var adminHeader, leaderHeader, regularHeader, displayHeader;
 
         regularHeader = <ul className="landing">
                             <Link to="/Home">
@@ -46,10 +46,27 @@ class Header extends React.Component {
                         </ul>
         ;
 
-        if(this.props.auth.user.userType !== ADMIN) {
-            displayHeader = regularHeader;
-        } else {
+        leaderHeader = <ul className="landing">
+                            <Link to="/Home">
+                                <div className="headerText">
+                                    <i className="material-icons">flare</i> 
+                                    HumbleHapnings
+                                </div>
+                            </Link>
+                            <li><log onClick={() => this.props.logoutUser()}>Logout</log></li>
+                            <li><Link to="/ChurchLeader" classname="a">Church Leader</Link></li>
+                            <li><Link to="/ChurchDirectory" classname="a">Churches</Link></li>
+                            <li><Link to="/Events" classname="a">Events</Link></li>
+                            <li><Link to="/UnderConstruction" classname="a">Social</Link></li>
+                        </ul>
+        ;
+
+        if(this.props.auth.user.userType == ADMIN) {
             displayHeader = adminHeader;
+        } else if (this.props.auth.user.userType == CHURCH_LEADER){
+            displayHeader = leaderHeader;
+        } else {
+            displayHeader = regularHeader;
         }
 
         return (
