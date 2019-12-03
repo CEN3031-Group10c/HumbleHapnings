9c80c60a228e1f9a04224790b162d319687182e1
Cluekas <lugilmir@gmail.com>
2019-12-03 12:07:34 -0500

    tests

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -6,11 +6,12 @@ var firebaseConfig = {
         authDomain: process.env.AUTH_DOMAIN,
         databaseURL: process.env.DATABASE_URL,
         projectId: process.env.PROJECT_ID,
-        storageBucket: process.env.STORAGE_BUCKET,
+        storageBucket: "humblehapningsimagedatabase.appspot.com",
         messagingSenderId: process.env.MESSAGING_SENDER_ID,
         appId: process.env.APP_ID,
         measurementId: process.env.MEASUREMENT_ID
 }
+console.log(firebaseConfig)
 
 firebase.initializeApp(firebaseConfig);
 
