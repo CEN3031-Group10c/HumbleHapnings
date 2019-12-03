efbb085505a0ce18ec19435c6929f8cc7322c1d9
shebasquine <40399062+llerenabarruetos@users.noreply.github.com>
2019-12-03 09:14:30 -0500

    heroku

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,16 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
     db: {
         uri: process.env.DB_URI, 
         secretOrKey: process.env.SECRETOR_KEY
     }
 };
-=======
-    db:
-    {
-        uri: process.env.DB_URI,
-        secretOrKey: process.env.SECRETOR_KEY
-    }
-};
-//for Heroku branch only, the values of these keys are on Heroku
->>>>>>> 348b31d6c63a076a7bcb6beee72f982b77478e8a
