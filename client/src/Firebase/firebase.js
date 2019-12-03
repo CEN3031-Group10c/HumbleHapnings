3174c98c2243066c50562acfab98e7d73da2313e
shebasquine <40399062+llerenabarruetos@users.noreply.github.com>
2019-12-03 09:03:35 -0500

    heroku test

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -4,14 +4,14 @@ import "firebase/storage";
 const config = require("./firebase-config");
 
 var firebaseConfig = {
-        apiKey: config.db.apiKey,
-        authDomain: config.db.authDomain,
-        databaseURL: config.db.databaseURL,
-        projectId: config.db.projectId,
-        storageBucket: config.db.storageBucket,
-        messagingSenderId: config.db.messagingSenderId,
-        appId: config.db.appId,
-        measurementId: config.db.measurementId
+        apiKey: process.env.apiKey,
+        authDomain: process.env.authDomain,
+        databaseURL: process.env.databaseURL,
+        projectId: process.env.projectId,
+        storageBucket: process.env.storageBucket,
+        messagingSenderId: process.env.messagingSenderId,
+        appId: process.env.appId,
+        measurementId: process.env.measurementId
 }
 
 firebase.initializeApp(firebaseConfig);
