828c70951a2fa6b94b75024e3e67870ed434a6f8
Cluekas <lugilmir@gmail.com>
2019-12-03 12:12:29 -0500

    revert

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -6,12 +6,11 @@ var firebaseConfig = {
         authDomain: process.env.AUTH_DOMAIN,
         databaseURL: process.env.DATABASE_URL,
         projectId: process.env.PROJECT_ID,
-        storageBucket: "humblehapningsimagedatabase.appspot.com",
+        storageBucket: process.env.STORAGE_BUCKET,
         messagingSenderId: process.env.MESSAGING_SENDER_ID,
         appId: process.env.APP_ID,
         measurementId: process.env.MEASUREMENT_ID
 }
-console.log(firebaseConfig)
 
 firebase.initializeApp(firebaseConfig);
 
