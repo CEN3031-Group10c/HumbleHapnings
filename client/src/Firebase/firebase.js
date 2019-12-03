643a8053bc2af5fe2d177c5ee9520e1010fb65b8
Cluekas <lugilmir@gmail.com>
2019-12-03 11:11:58 -0500

    Changed firebase config for heroku

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -1,15 +1,17 @@
 import firebase from "firebase/app";
 import "firebase/storage";
 
+const config = require("./firebase-config");
+
 var firebaseConfig = {
-        apiKey: process.env.apiKey,
-        authDomain: process.env.authDomain,
-        databaseURL: process.env.databaseURL,
-        projectId: process.env.projectId,
-        storageBucket: process.env.storageBucket,
-        messagingSenderId: process.env.messagingSenderId,
-        appId: process.env.appId,
-        measurementId: process.env.measurementId
+        apiKey: config.db.apiKey,
+        authDomain: config.db.authDomain,
+        databaseURL: config.db.databaseURL,
+        projectId: config.db.projectId,
+        storageBucket: config.db.storageBucket,
+        messagingSenderId: config.db.messagingSenderId,
+        appId: config.db.appId,
+        measurementId: config.db.measurementId
 }
 
 firebase.initializeApp(firebaseConfig);
