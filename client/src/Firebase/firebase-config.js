b7709812aa22d35ef2c1ffe0fd38e4ad1bcdbf57
Cluekas <lugilmir@gmail.com>
2019-12-03 11:25:27 -0500

    Changed config vars heroku

--- client/src/Firebase/firebase-config.js
+++ client/src/Firebase/firebase-config.js
@@ -1,12 +1,12 @@
 module.exports = {
     db: {
-        apiKey: process.env.apiKey,
-        authDomain: process.env.authDomain,
-        databaseURL: process.env.databaseURL,
-        projectId: process.env.projectId,
-        storageBucket: process.env.storageBucket,
-        messagingSenderId: process.env.messagingSenderId,
-        appId: process.env.appId,
-        measurementId: process.env.measurementId
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID
     }
 };
