6e70aa5a387b6a18125d1f62236e2ea60cb0a714
Cluekas <lugilmir@gmail.com>
2019-12-03 15:05:11 -0500

    Heroku test

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -12,7 +12,16 @@ var firebaseConfig = {
         measurementId: process.env.MEASUREMENT_ID
 }
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID
+});
 
 var storage = firebase.storage();
 
