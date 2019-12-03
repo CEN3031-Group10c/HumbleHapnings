83c5ad64295dbe80a96649372a647fdca7e76018
Cluekas <lugilmir@gmail.com>
2019-12-03 17:21:44 -0500

    Revert

--- client/src/Firebase/firebase.js
+++ client/src/Firebase/firebase.js
@@ -3,14 +3,14 @@ import "firebase/storage";
 require('dotenv').load();
 
 var firebaseConfig = {
-        apiKey: "AIzaSyD__LTezaxAoVDpFch92GSLHIPxCO5OWKc",
-        authDomain: "humblehapningsimagedatabase.firebaseapp.com",
-        databaseURL: "https://humblehapningsimagedatabase.firebaseio.com",
-        projectId: "humblehapningsimagedatabase",
-        storageBucket: "humblehapningsimagedatabase.appspot.com",
-        messagingSenderId: "834145157074",
-        appId: "1:834145157074:web:0829b0422a56a8a57c520b",
-        measurementId: "G-02JC5CCH8Q"
+        apiKey: process.env.API_KEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID
 }
 
 firebase.initializeApp({firebaseConfig});
