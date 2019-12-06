53ea9acf3d6c47aa7d744d0a87b7f8d0d6d6c260
mfowler38 <fowler.megan@hotmail.com>
2019-12-06 03:32:06 -0500

    server attempt

--- server/server.js
+++ server/server.js
@@ -34,5 +34,13 @@ app.use("/api/users", users);
 // Use env port or default
 const port = process.env.PORT || 5000;
 
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 
 app.listen(port, () => console.log(`Server now running on port ${port}!`));
